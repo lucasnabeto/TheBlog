6027936d26cbab51deea9017864bdd1adc1006d8
Lucas Nabeto <lucas.nabeto@outlook.com>
2023-09-26 22:50:58 -0300

    feat: updated home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NewsContent, fakeData } from 'src/app/data/fake-data';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  mainNews: NewsContent = fakeData
+    .filter((news) => news.mainNews === true)
+    .sort((news1, news2) =>
+      news1.publicationDate < news2.publicationDate
+        ? 1
+        : news2.publicationDate < news1.publicationDate
+        ? -1
+        : 0
+    )[0];
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { MainNewsComponent } from './components/main-news/main-news.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent, MainNewsComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class HomeModule {}
